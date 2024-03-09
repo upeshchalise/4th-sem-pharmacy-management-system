63299fac00bb38544e01e604919ec603c4a2d1f5
upeshchalise <upeshchalise@gmail.com>
2024-03-09 18:11:58 +0545

    added user create and find user by id routes

---
+++ src/middleware/validator.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response, NextFunction } from "express";
+
+export const validate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { createUser, getAllUser } from "../controllers/user.controller";
+import {
+  createUser,
+  getAllUser,
+  getUserById,
+} from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.get("/allUser", getAllUser);
 router.post("/createUser", createUser);
+router.get("/:id", getUserById);
 
 module.exports = router;
